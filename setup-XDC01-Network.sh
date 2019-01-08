c53f186e33ae0d3911d6e589160f2daa92987818
AnilChinchawale <Anil24593@gmail.com>
2019-01-08 12:42:43 +0530

    Network inputs queries added.

--- setup-XDC01-Network.sh
+++ setup-XDC01-Network.sh
@@ -6,4 +6,16 @@
 
 echo "[*] Init XinFin DPoS Network"
 
+echo "[*] Please enter Project/Network name :- "
+read project_name
+
+echo "[*]Specify your chain/network ID if you want an explicit one (default = random)"
+read network_id
+
+PROJECT_ROOT_DIR=${project_name}_network
+PROJECT_CUSTOM_GENESIS_FILE=${project_name}_genesis.json
+
+mkdir $PROJECT_ROOT_DIR
+cd $PROJECT_ROOT_DIR
+mkdir logs
 
