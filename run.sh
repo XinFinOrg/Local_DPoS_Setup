69a3e18cf2da2ad97b1e008b2c6c17ded6afb335
AnilChinchawale <anil24593@gmail.com>
2019-02-07 15:39:44 +0530

    Stats added in startup script

--- run.sh
+++ run.sh
@@ -9,11 +9,11 @@ trap _interupt INT TERM
 
 touch .pwd
 export $(cat .env | xargs)
-Bin_NAME=gxdc
+Bin_NAME=XDC
 
 WORK_DIR=$PWD
-PROJECT_DIR="${HOME}/go/src/github.com/ethereum/go-ethereum"
-cd $PROJECT_DIR && make gxdc
+PROJECT_DIR="/root/XinFin/XDPoS-TestNet-Apothem"
+cd $PROJECT_DIR && make all
 cd $WORK_DIR
 
 if [ ! -d ./nodes/1/$Bin_NAME/chaindata ]
@@ -33,13 +33,9 @@ fi
 VERBOSITY=3
 GASPRICE="1"
 
-echo Starting the bootnode ...
-${PROJECT_DIR}/build/bin/bootnode -nodekey ./bootnode.key &
-child_proc=$! 
-
 echo Starting the nodes ...
-${PROJECT_DIR}/build/bin/$Bin_NAME --bootnodes "enode://7d8ffe6d28f738d8b7c32f11fb6daa6204abae990a842025b0a969aabdda702aca95a821746332c2e618a92736538761b1660aa9defb099bc46b16db28992bc9@127.0.0.1:30301" --syncmode "full" --datadir ./nodes/1 --networkid 89 --port 30303 --rpc --rpccorsdomain "*" --ws --wsaddr="0.0.0.0" --wsorigins "*" --rpcaddr 0.0.0.0 --rpcport 8545 --rpcvhosts "*" --unlock "${wallet1}" --password ./.pwd --mine --gasprice "${GASPRICE}" --targetgaslimit "420000000" --verbosity ${VERBOSITY} --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,istanbul &
+${PROJECT_DIR}/build/bin/$Bin_NAME --bootnodes "enode://1c20e6b46ce608c1fe739e78611225b94e663535b74a1545b1667eac8ff75ed43216306d123306c10e043f228e42cc53cb2728655019292380313393eaaf6e23@5.152.223.197:30303" --syncmode "full" --datadir ./nodes/1 --networkid 1993 --port 30306 --rpc --rpccorsdomain "*" --ws --wsaddr="0.0.0.0" --wsorigins "*" --rpcaddr 0.0.0.0 --rpcport 8549 --rpcvhosts "*" --unlock "${wallet1}" --password ./.pwd --mine --gasprice "${GASPRICE}" --targetgaslimit "420000000" --verbosity ${VERBOSITY} --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,istanbul,XDPoS --ethstats "BlockWorks01:xinfin_network_stats@stats.testnet.xinfin.network:3001" &
 child_proc="$child_proc $!"
-${PROJECT_DIR}/build/bin/$Bin_NAME --bootnodes "enode://7d8ffe6d28f738d8b7c32f11fb6daa6204abae990a842025b0a969aabdda702aca95a821746332c2e618a92736538761b1660aa9defb099bc46b16db28992bc9@127.0.0.1:30301" --syncmode "full" --datadir ./nodes/2 --networkid 89 --port 30304 --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcport 8556 --rpcvhosts "*" --unlock "${wallet2}" --password ./.pwd --mine --gasprice "${GASPRICE}" --targetgaslimit "420000000" --verbosity ${VERBOSITY} --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,istanbul &
+${PROJECT_DIR}/build/bin/$Bin_NAME --bootnodes "enode://1c20e6b46ce608c1fe739e78611225b94e663535b74a1545b1667eac8ff75ed43216306d123306c10e043f228e42cc53cb2728655019292380313393eaaf6e23@5.152.223.197:30303" --syncmode "full" --datadir ./nodes/2 --networkid 1993 --port 30304 --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcport 8556 --rpcvhosts "*" --unlock "${wallet2}" --password ./.pwd --mine --gasprice "${GASPRICE}" --targetgaslimit "420000000" --verbosity ${VERBOSITY} --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,istanbul,XDPoS --ethstats "BlockWorks02:xinfin_network_stats@stats.testnet.xinfin.network:3001" &
 child_proc="$child_proc $!"
-${PROJECT_DIR}/build/bin/$Bin_NAME --bootnodes "enode://7d8ffe6d28f738d8b7c32f11fb6daa6204abae990a842025b0a969aabdda702aca95a821746332c2e618a92736538761b1660aa9defb099bc46b16db28992bc9@127.0.0.1:30301" --syncmode "full" --datadir ./nodes/3 --networkid 89 --port 30305 --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcport 8547 --rpcvhosts "*" --unlock "${wallet3}" --password ./.pwd --mine --gasprice "${GASPRICE}" --targetgaslimit "420000000" --verbosity ${VERBOSITY} --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,istanbul
+${PROJECT_DIR}/build/bin/$Bin_NAME --bootnodes "enode://1c20e6b46ce608c1fe739e78611225b94e663535b74a1545b1667eac8ff75ed43216306d123306c10e043f228e42cc53cb2728655019292380313393eaaf6e23@5.152.223.197:30303" --syncmode "full" --datadir ./nodes/3 --networkid 1993 --port 30305 --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcport 8547 --rpcvhosts "*" --unlock "${wallet3}" --password ./.pwd --mine --gasprice "${GASPRICE}" --targetgaslimit "420000000" --verbosity ${VERBOSITY} --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,istanbul,XDPoS --ethstats "BlockWorks03:xinfin_network_stats@stats.testnet.xinfin.network:3001" 
